456f2f49e588c5cf316c35ab3c94d397eb3648a5
G Shreekar <144794118+GShreekar@users.noreply.github.com>
2025-05-07 20:07:24 +0530

    Parse User Agent

--- main/main.py
+++ main/main.py
@@ -11,14 +11,34 @@ def main():
                 continue
             request = data.decode("utf-8", errors="ignore")
             print("Request:", request)
-            request = request.splitlines()[0]
-            parts = request.split()
+            status = request.splitlines()[0]
+            parts = status.split()
             if len(parts) >= 2:
                 path = parts[1]
             else:
                 path = ""
             if path == "/":
                 connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            elif path.startswith("/echo"):
+                content = path[6:]
+                response = (
+                    f"HTTP/1.1 200 OK\r\n"
+                    f"Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(content)}\r\n"
+                    f"\r\n"
+                    f"{content}"
+                )
+                connection.sendall(response.encode("utf-8"))
+            elif path == "/user-agent":
+                userAgent = request.split("User-Agent: ")[1].split("\r\n")[0]
+                response = (
+                    f"HTTP/1.1 200 OK\r\n"
+                    f"Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(userAgent)}\r\n"
+                    f"\r\n"
+                    f"{userAgent}"
+                )
+                connection.sendall(response.encode("utf-8"))
             else:
                 connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
